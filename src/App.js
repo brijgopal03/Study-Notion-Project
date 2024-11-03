385569d865f3d0ada03da345f738510478bd5057
brijgopal03 <brijgopaldalmia2003@gmail.com>
2024-11-03 20:38:38 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import Dashboard from "./pages/Dashboard"
+import { useState } from "react";
 
 function App() {
-  return <div>Hello World</div>;
-}
+
+const [isLoggedIn, setisLoggedIn] = useState(false)
+   
+  return (
+    <div>
+    <Navbar isLoggedIn = {isLoggedIn} setisLoggedIn = {setisLoggedIn} />
+
+
+<Routes>
+  <Route path="/" element = {<Home/>} />
+  <Route path="/login" element = {<Login/>} />
+  <Route path="/dashboard" element = {<Dashboard/>} />
+  <Route path="/signup" element = {<Signup/>} />
+
+</Routes>
+
+  </div>
+  ) 
+};
 
 export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/Logo.svg'
+import toast from 'react-hot-toast'
+
+const Navbar = (props) => {
+  let isLoggedin = props.isLoggedin;
+  let setisLoggedIn = props.setisLoggedIn;
+  return (
+    <div className='flex justify-evenly'>
+        <Link to="/"> <img src={Logo} alt="logo" width={160} height={32} loading='lazy' /> </Link>
+        <nav>
+          <ul className='flex gap-3'>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to = "/">Contact</Link>
+            </li>
+            <li>
+              <Link to = "/">About</Link>
+            </li>
+            
+          </ul>
+        </nav>
+
+<div className='flex gap-3 ml-5'>
+  { !isLoggedin && <Link to = "/login" ><button>Login</button></Link>}
+  {!isLoggedin && <Link to = "/signup" ><button>SignUp</button></Link>}
+  {isLoggedin &&  <Link to = "/"><button onClick={()=>{setisLoggedIn(false); toast.success("Logged Out!");}}>Log Out</button></Link> }
+  {isLoggedin &&  <Link to = "/dashboard"><button>Dashboard</button></Link>}
+</div>
+
+    </div>
+  )
+}
+
+export default Navbar
